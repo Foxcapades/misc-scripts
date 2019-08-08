9039c06b63a3a0408acb7502c7ab5c6e91b35159
Elizabeth Harper <epharper@upenn.edu>
2019-08-08 10:31:24 -0400

    moved the post-checkout steps to a separate script so they can be called separately

--- eupath-svn-2-git/sco.sh
+++ eupath-svn-2-git/sco.sh
@@ -3,9 +3,6 @@
 # Expected messages and errors that can be skipped
 SKIPLIST='^([\s\t]|Found (merge|branch|possible)|Following|W: ([\-+]empty_dir|Refspec|Ignoring|Do not be alarmed)|expected path:|This may|Continuing|Initializing|Use of|Successfully)'
 
-# Github org for the git mirrors
-ORGANIZATION="EuPathDB"
-
 HOME=$1
 PROJECT=$2
 
@@ -27,30 +24,13 @@ function showUsage() {
   sco.sh <(gus|apidb)> <{project_name}>"
 }
 
-# Git checkout all the svn remote branches to prep them all for pushing to git
-function prepBranches() {
-  for i in `git branch --remotes | sed 's/origin\///;s/ //'`; do
-    git checkout $i;
-  done
-}
-
-# Perform any post-svn-checkout, pre-git-push cleanup steps
-function cleanup() {
-  git branch -D trunk
-}
-
 # Verify inputs
 if [[ -z ${HOME} ]] || [[ -z ${PROJECT} ]]; then
  showUsage
  exit 1
 fi;
 
-
 # Perform the checkout
-gitsvn $HOME $PROJECT 2>&1 | grep -Pv "${SKIPLIST}" \
-  && cd $PROJECT \
-  && prepBranches \
-  && cleanup \
-  && git push --all origin -fu \
-  && cd ..
-
+gitsvn $HOME $PROJECT 2>&1 \
+  | grep -Pv "${SKIPLIST}" \
+  && ./post-checkout $PROJECT
